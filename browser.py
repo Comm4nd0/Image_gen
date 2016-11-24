d20372bbaa5aa7f487a67df4db199ee79bdbdf7d
Comm4nd0 <marcobaldanza@hotmail.co.uk>
2016-11-24 23:26:44 +0000

    updated web page
    and so much more!

--- browser.py
+++ browser.py
@@ -7,76 +7,65 @@
 import os
 import praw
 import webbrowser
+import urllib3
 
-ANA_FORMAT = ['.mp4', '.webm']
+from bs4 import BeautifulSoup
+
+ANA_FORMAT = ['.mp4', '.webm', '.gifv', '.jpg', '.png', '.jpeg', '.bmp']
+
 
 class createWebpage():
     def __init__(self):
         self.image_paths = []
-        self.image_urls = ""
+        self.image_html = ""
+        self.img_thumbnails = []
         self.web = praw.Reddit(user_agent='gimmy ana_pics')
-        del self.image_paths[:]
+        self.http = urllib3.PoolManager()
 
     def get_urls(self, subreddit):
+        self.clear_cache()
         self.subreddit = self.web.get_subreddit(subreddit).get_hot(limit=100)
         for item in self.subreddit:
             if os.path.splitext(item.url)[1] in ANA_FORMAT:
                 self.image_paths.append(item.url)
-                print(item.url)
-        print(len(self.image_paths))
-        self.makeHTML()
+                self.img_thumbnails.append(item.thumbnail)
+            elif 'gfycat.com' in item.url:
+                self.get_gfycat(item.url)
+                self.img_thumbnails.append(item.thumbnail)
+        if len(self.image_paths) == 0:
+            return 0
+        else:
+            self.makeHTML()
+            return 1
+
+    def clear_cache(self):
+        del self.image_paths[:]
+        del self.img_thumbnails[:]
 
     def makeHTML(self):
+        num = 0
         for url in self.image_paths:
-            type = os.path.splitext(url)[1]
-            type = type.replace('.', '')
-            self.image_urls += '<source src="' + url + '" type="video/' + type + '">'
-            print(self.image_urls)
-
+            self.image_html += '<a href="' + url + '" data-poster="' + self.img_thumbnails[num] + '" data-webm="' + url + '"><img src="' + self.img_thumbnails[num] + '"  width="250px" height="250px"></a>'
+            num += 1
 
         self.html = """<!DOCTYPE html>
-        <html>
-        <title>Images </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+        <html><head><script type="text/javascript" id="www-widgetapi-script" src="test_files/www-widgetapi.js" async=""></script>
+            <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+            <meta name="viewport" content="width=device-width">
+            <title>Image gallery</title>
+
+
+
         <body>
-
-        <h2 class="w3-center">Anamated Images</h2>
-
-        <div class="w3-content w3-display-container">
-        <video autoplay
+        <a href="initial.jpg" style="display: none;"><img src="initial.jpg" alt="Waves" data-description="Minions!"></a>
         """
 
-        self.html += self.image_urls
+        self.html += self.image_html
 
         self.html += """
-        </video>
-          <a class="w3-btn-floating w3-display-left" onclick="plusDivs(-1)">&#10094;</a>
-          <a class="w3-btn-floating w3-display-right" onclick="plusDivs(1)">&#10095;</a>
-        </div>
-
-        <script>
-        var slideIndex = 1;
-        showDivs(slideIndex);
-
-        function plusDivs(n) {
-          showDivs(slideIndex += n);
-        }
-
-        function showDivs(n) {
-          var i;
-          var x = document.getElementsByClassName("mySlides");
-          if (n > x.length) {slideIndex = 1}
-          if (n < 1) {slideIndex = x.length}
-          for (i = 0; i < x.length; i++) {
-             x[i].style.display = "none";
-          }
-          x[slideIndex-1].style.display = "block";
-        }
-        </script>
-
-        </body>
-        </html>"""
+        <a href="initial.jpg" style="display: none;"><img src="initial.jpg" alt="Waves" data-description="Minions!"></a>
+        </body></html>
+        """
         with open('images.html', 'w') as webpage:
             webpage.write(self.html)
             webpage.close()
@@ -84,3 +73,12 @@ class createWebpage():
 
     def loadBrowser(self):
         webbrowser.open('images.html')
+
+    def get_gfycat(self, url):
+        req = self.http.request('GET', url)
+        soup = BeautifulSoup(req.data, "lxml")
+        scrape = soup.find_all(id="mp4Source")
+        scrape = str(scrape[0])
+        start = scrape.find('https://')
+        end = scrape.find('.mp4') + 4
+        self.image_paths.append(scrape[start:end])
